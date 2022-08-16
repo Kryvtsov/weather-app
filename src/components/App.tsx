92eceb73262462b0adff44bfbed1180db7cde802
Volodymyr <Volodymyr.Kryvtsov-external@sema4genomics.com>
2022-08-16 17:54:44 -0400

    feat: add empty project

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+// import Weather from './Weather';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Show Local Weather</h1>
+				</header>
+
+				{/* <Weather /> */}
+			</div>
+		);
+	}
+}
+
+export default App;
